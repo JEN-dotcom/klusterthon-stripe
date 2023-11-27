90cdbf967ad797c2559100f35b9e85a30b6befe8
unknown <johnenyinwa@gmail.com>
2023-11-27 03:04:58 +0100

    added home route

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.json());
 
 app.use(
     cors({
-        origin: [ "http://localhost:5173, https://klusterthon.vercel.app"],
+        origin: [ "http://localhost:5173, http://localhost:5173/, http://127.0.0.1:5173, https://klusterthon.vercel.app"],
         methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     })
 )
@@ -20,7 +20,7 @@ app.get('/ping', (req, res) => {
 })
 
 app.get('/', (req, res) => {
-    res.send('stripe integrated')
+    res.send('stripe1 integrated')
 })
 
 app.post("/create-checkout-session", async (req, res) => {
